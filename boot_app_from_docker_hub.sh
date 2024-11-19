4c35ee5c8d04da01370b21843ccdece640fe3acd
mruggeri-flx <martin.ruggeri@flexibility.com.ar>
2024-11-19 15:38:17 -0300

    docker hub

---
+++ boot_app_from_docker_hub.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo docker pull martinr2020/temira-sleep-detector:latest
+
+sudo docker run --rm --device=/dev/video0:/dev/video0 --device /dev/snd -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/shm:/dev/shm -v $(pwd)/logs:/app/logs -it martinr2020/temira-sleep-detector:latest
