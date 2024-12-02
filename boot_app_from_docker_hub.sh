b3cd79b91963b2c9495b15de87cf2fb200273dea
mruggeri-flx <martin.ruggeri@flexibility.com.ar>
2024-12-02 16:43:54 -0300

    fix docker

--- boot_app_from_docker_hub.sh
+++ boot_app_from_docker_hub.sh
@@ -4,4 +4,4 @@ sudo docker pull martinr2020/temira-sleep-detector:2.0.0
 
 sudo xhost +
 
-sudo docker run --rm --device=/dev/video0:/dev/video0 --device /dev/snd -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/shm:/dev/shm -v $(pwd)/logs:/app/logs -v $(pwd)/checksum:/app/checksum -it martinr2020/temira-sleep-detector:latest
+sudo docker run --rm --device=/dev/video0:/dev/video0 --device /dev/snd -e DISPLAY=$DISPLAY -v /tmp/.X11-unix:/tmp/.X11-unix -v /dev/shm:/dev/shm -v $(pwd)/logs:/app/logs -v $(pwd)/checksum:/app/checksum -it martinr2020/temira-sleep-detector:2.0.0
