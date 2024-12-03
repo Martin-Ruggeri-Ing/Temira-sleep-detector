56a2aa8c581798d814551afd1fa86eb37a2584b7
mruggeri-flx <martin.ruggeri@flexibility.com.ar>
2024-12-03 15:00:55 -0300

    mejora checksums

--- boot_app_from_docker_hub.sh
+++ boot_app_from_docker_hub.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 xhost +local:
 
-sudo docker pull martinr2020/temira-sleep-detector:v2.0.0
+sudo docker pull martinr2020/temira-sleep-detector:v3.0.0
 
 sudo docker run --rm \
+    --privileged \
     --device=/dev/video0:/dev/video0 \
     --device /dev/snd:/dev/snd \
     -e DISPLAY=$DISPLAY \
@@ -11,10 +12,10 @@ sudo docker run --rm \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /dev/shm:/dev/shm \
     -v $(pwd)/logs:/app/logs \
-    -v $(pwd)/checksum:/app/checksum \
+    -v $(pwd)/checksums:/app/checksums \
     -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
     -v /tmp/pulse-native:/tmp/pulse-native \
     --network host \
-    -it martinr2020/temira-sleep-detector:v2.0.0
+    -it martinr2020/temira-sleep-detector:v3.0.0
 
 xhost -local:
